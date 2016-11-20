7bb13e7aca5d6cae2de3d941ef5c656074c6a856
James Lovejoy <jameslovejoy1@gmail.com>
2016-11-20 16:51:07 -0500

    Demonstrate class bindings

--- main.cpp
+++ main.cpp
@@ -8,11 +8,29 @@ void programCounterHook(lua_State* luaState, lua_Debug* event)
     std::cout << "pc: " << pc << std::endl;
 }
 
+class StringVector
+{
+    public:
+        StringVector() {}
+        void push(const std::string val)
+        {
+            vec.push_back(val);
+        }
+        std::string get(const int index)
+        {
+            return vec[index];
+        }
+    private:
+        std::vector<std::string> vec;
+};
+
 int main()
 {
     sel::State state;
 
     lua_sethook(state.getState(), &programCounterHook, LUA_MASKCOUNT, 1);
+
+    state["StringVector"].SetClass<StringVector>("push", &StringVector::push, "get", &StringVector::get);
 
     //state.loadString("function test()\n    return 4\nend\n");
     state.Load("./test.lua");
--- test.lua
+++ test.lua
@@ -1,3 +1,13 @@
 function test()
-    return 4
+    x = StringVector.new()
+
+    x.push("test")
+    x.push("andanotherone")
+    x.push("wowowowow")
+
+    if x.get(0) == "test" then
+        return 0
+    else
+        return 4
+    end
 end
