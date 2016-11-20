fc292a9fde9c949e2bf66a4abe28901d50875e89
James Lovejoy <jameslovejoy1@gmail.com>
2016-11-20 14:49:45 -0500

    Implement a basic program counter

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <selene.h>
 
+int pc = 0;
+
+void programCounterHook(lua_State* luaState, lua_Debug* event)
+{
+    pc++;
+    std::cout << "pc: " << pc << std::endl;
+}
+
 int main()
 {
     sel::State state;
 
-    state.loadString("function test()\n    return 4\nend\n");
+    lua_sethook(state.getState(), &programCounterHook, LUA_MASKCOUNT, 1);
+
+    //state.loadString("function test()\n    return 4\nend\n");
+    state.Load("./test.lua");
 
     std::cout << (int)state["test"]();
 
